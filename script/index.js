4efcda126d7c30c163c1e523d49b1c38850dbc86
Ivan Ushachov <90464856+VanyaSO@users.noreply.github.com>
2022-07-13 11:55:01 +0300

    Update index.js

--- script/index.js
+++ script/index.js
@@ -1,51 +1,43 @@
-// Task #1
-// Реализовать рекурсивную функцию которая находит факториал переданного в нее числа
-// getFactorial(3) // в данном случае должна вернуть факториал числа 3! = 3 * 2 * 1
+"use strict";
 
-function getFactorial(num){
 
-    if(num === 0 || num === 1){
-        return 1;
+let user = {
+    data: {
+        a: 1,
+        b: 2,
+        c: 3,
+        d: {
+            a1: 1,
+            b1: 2,
+            c1: 3,
+            d1: {
+                a2: 3,
+                b2: 3,
+                c2: 3,
+            }
+        },
     }
-
-    return num * getFactorial(num - 1);
 }
 
-console.log('Task #1');
-console.log(getFactorial(3));
+function DeepFreeze(obj){
 
+    for(let item in obj){
+        console.log(item);
 
-// Task #2
-// Реализовать рекурсивную функцию которая находит возводит число в степень.
-// Число которое нужно возвести в степень передается как первый аргумент в функцию
-// Степень передается как второй аргумент в функцию pow(num, degree)
+        if (typeof obj[item] === "object"){
+            Object.freeze(obj[item]);
+            return DeepFreeze(obj[item]);
+        }
 
-function pow(num, degree){
-
-    if(degree === 1){
-        return num;
     }
 
-    return num * pow(num, degree - 1);
-}
-
-console.log('Task #2');
-console.log(pow(3,5));
-
-// Task #3
-// Рекурсивное суммирование
-// Задача: напишите рекурсивную функцию для вычисления суммы заданных положительных целых чисел a и b
-// без прямого использования оператора +.
-
-function sum(a, b){
-
-    if(a === 0){
-        return b;
-    }
-
-    return sum(a - 1,b + 1);
+    return;
 
 }
 
-console.log('Task #3');
-console.log(sum(3,5));
+DeepFreeze(user);
+
+//проверка на freez
+user.data.a = 454545;
+
+console.log(user.data.a);
